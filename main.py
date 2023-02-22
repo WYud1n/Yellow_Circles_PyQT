6ba2ec5d577c3704dbe7570bced60025bc0ccd91
WYud1n <bevqntb@gmail.com>
2023-02-22 21:21:43 +0300

    Added ui.py and now circles are multicolored

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
 from random import randrange
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtCore import QPoint
 
+from ui import Ui_MainWindow
 
-class Example(QMainWindow):
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
+
         self.do_paint = False
         self.pushButton.clicked.connect(self.paint)
 
@@ -26,7 +28,7 @@ class Example(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 204, 0))
+        qp.setBrush(QColor(randrange(0, 255), randrange(0, 255), randrange(0, 255)))
         r = randrange(0, 600)
         p = QPoint(randrange(0, 1280), randrange(0, 720))
         qp.drawEllipse(p, r, r)
