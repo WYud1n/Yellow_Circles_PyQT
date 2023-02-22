7b4965bebe3e55e7421e11483c58d0862a1ac8fd
WYud1n <bevqntb@gmail.com>
2023-02-22 21:04:42 +0300

    Added UI.ui and finished application in main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+from random import randrange
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import QPoint
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 204, 0))
+        r = randrange(0, 600)
+        p = QPoint(randrange(0, 1280), randrange(0, 720))
+        qp.drawEllipse(p, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
